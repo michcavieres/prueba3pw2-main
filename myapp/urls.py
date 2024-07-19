f1ff66a9f79b5a3bd4799bebfdddcf8b68603c7f
Nat9210 <nata.benitez.pra@gmail.com>
2024-07-19 18:09:18 -0400

    agregar catalogo2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index,home,login,categories,news,newsdetails,animedetails,animewatching,crud,blog,exit,registro,perfil,create_crud,detail_crud
+from .views import index,home,login,categories,news,newsdetails,animedetails,animewatching,crud,blog,exit,registro,perfil,create_crud,detail_crud, delete_crud
 
 
 urlpatterns = [
@@ -16,7 +16,7 @@ urlpatterns = [
  path('crud/', crud, name='crud'),
  path('crud/create/', create_crud, name = 'create_crud'),
  path('crud/<int:media_id>/',detail_crud, name = 'detail_crud'),
- 
+ path('crud/<int:media_id>/',delete_crud, name = 'delete_crud'),
  path('blog/', blog, name='blog'),
  path('logout/', exit, name='exit'),
  
